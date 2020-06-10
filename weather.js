9e2fa5a83ade1d23721848879140eecd1a0c8bb3
Kim Min Ji <didtjdrla@gmail.com>
2020-06-11 03:29:52 +0900

    Add Commit

--- weather.js
+++ weather.js
@@ -10,7 +10,7 @@ function getWeather(lat, lng){
     }).then(function(json) {
       const temperature = json.main.temp;
       const place = json.name;
-      weather.innerHTML = `${temperature}<br>@${place}`
+      weather.innerHTML = `${temperature}º<br>@${place}`
     })
 }
 
