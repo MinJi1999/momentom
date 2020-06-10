cd54fb06e584804f5a85acecde69d15a3e116732
Kim Min Ji <didtjdrla@gmail.com>
2020-06-11 03:27:45 +0900

    Add Commit

--- clock.js
+++ clock.js
@@ -7,10 +7,10 @@ function getTime(){
   const hours = date.getHours();
   const seconds = date.getSeconds();
   clockTitle.innerText = `${
-    hours < 10 ? `0${hours}` : hours} : ${
-    minutes < 10 ? `0${minutes}` : minutes} : ${
+    hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes}:${
     seconds < 10 ? `0${seconds}` : seconds}`;
-  
+
 }
 
 
